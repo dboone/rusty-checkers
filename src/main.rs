ca2ef277695d9a167f2fe7d61a55bbf125586e7f
dboone <boonesoftware@gmail.com>
2015-06-20 14:28:06 -0400

    Added Pieces and Occupied Tile
    Added structs ManPiece and KingPiece.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,38 @@
 use std::ops::Deref;
 
+pub trait Piece {
+}
+
+pub struct ManPiece;
+
+impl Piece for ManPiece {
+}
+
+pub struct KingPiece;
+
+impl Piece for KingPiece {
+}
+
 pub trait Tile {
+    fn get_piece(&self) -> Option<&Piece>;
 }
 
 pub struct EmptyTile;
 
 impl Tile for EmptyTile {
-    // Tile functions
+    fn get_piece(&self) -> Option<&Piece> {
+       Option::None
+    }
+}
+
+pub struct OccupiedTile {
+    piece : Box<Piece>
+}
+
+impl Tile for OccupiedTile {
+    fn get_piece(&self) -> Option<&Piece> {
+       Option::Some(self.piece.deref())
+    }
 }
 
 pub struct Board {
@@ -15,27 +41,26 @@ pub struct Board {
     tiles : Vec<Box<Tile>>
 }
 
-const CHECKERS_BOARD_SIZE : usize = 8;
-const CHECKERS_NUMBER_TILES : usize = CHECKERS_BOARD_SIZE * CHECKERS_BOARD_SIZE;
+const CHECKERBOARD_SIZE : usize = 8;
+const CHECKERS_NUMBER_TILES : usize = CHECKERBOARD_SIZE * CHECKERBOARD_SIZE;
 
 impl Board {
     fn new() -> Board {
-       let mut board = Board {
-           number_rows : CHECKERS_BOARD_SIZE,
-	   number_columns : CHECKERS_BOARD_SIZE,
-	   tiles : Vec::with_capacity(CHECKERS_NUMBER_TILES)
-       };
+        let mut board = Board {
+            number_rows : CHECKERBOARD_SIZE,
+            number_columns : CHECKERBOARD_SIZE,
+            tiles : Vec::with_capacity(CHECKERS_NUMBER_TILES)
+        };
 
-       // initialize tiles
-       for _ in 0..CHECKERS_NUMBER_TILES {
-           board.tiles.push(Box::new(EmptyTile));
-       }
+        for _ in 0..CHECKERS_NUMBER_TILES {
+            board.tiles.push(Box::new(EmptyTile));
+        }
 
-       board
+        board
     }
 
-    fn get_tile(&self, row: usize, column: usize) -> &Tile {
-       self.tiles[ row + self.number_rows * column ].deref()
+    fn get_tile(&self, row : usize, column : usize) -> &Tile {
+        self.tiles[ row + self.number_rows * column ].deref()
     }
 }
 
