3a0bc96ae219aa3844b1f19ddd951101406612bc
Dale Bassett <dalebassett9@gmail.com>
2015-06-22 22:59:26 -0400

    Added a player ID field to Pieces

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 mod checkers;
 use checkers::Board;
+use checkers::Player;
 
 fn main() {
     println!("Welcome to Draughts!");
 
-    let board = Board::new_checkerboard();
+	let player1 = Player{ id : 0 };
+	let player2 = Player{ id : 1 };
+    let board = Board::new_checkerboard(&player1, &player2);
     let tile = board.get_tile(0, 0);    
 }
