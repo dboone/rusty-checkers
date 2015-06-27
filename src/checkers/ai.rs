c89370635b2f3fd1fe53b0a3a2d3d905f22b80b4
drbassett <dale.r.bassett@gmail.com>
2015-06-27 16:14:15 -0400

    Added functions for finding available moves
    This includes finding the simple moves for both man and king piece types.

--- src/checkers/ai.rs
+++ src/checkers/ai.rs
@@ -1,3 +1,115 @@
+pub enum Direction {
+	/// The piece is moving such that its rank is increasing
+	IncreasingRank,
+
+	/// The piece is moving such that its rank is decreasing
+	DecreasingRank
+}
+
+// A move from one tile to an adjacent diagonal one
+pub struct SimpleMove {
+	to_row : usize,
+	to_col : usize
+}
+
+/// Given the position of a main piece on a board, and the
+/// direction this man piece is moving, determines the simple
+/// moves available to this piece.
+pub fn find_simple_moves_for_man
+(board : &super::Board,
+		direction : Direction,
+		row : usize,
+		col : usize)
+-> Vec<SimpleMove> {
+	let row_offset = match direction {
+		Direction::IncreasingRank => TileOffset::Negative(1),
+		Direction::DecreasingRank => TileOffset::Positive(1),
+	};
+	
+	let mut moves = Vec::new();
+
+	{
+		let col_offset = TileOffset::Negative(1);
+		push_simple_move_if_valid(
+			board, row, col, &row_offset, &col_offset, &mut moves);
+	}
+
+	{
+		let col_offset = TileOffset::Positive(1);
+		push_simple_move_if_valid(
+			board, row, col, &row_offset, &col_offset, &mut moves);
+	}
+
+	moves
+}
+
+/// Given the position of a king piece on a board, determines
+/// the simple moves available to this piece.
+///
+/// This function does not require a Direction like the
+/// find_simple_moves_for_man function, because kings can move
+/// in all directions.
+pub fn find_simple_moves_for_king
+(board : &super::Board,
+		row : usize,
+		col : usize)
+-> Vec<SimpleMove> {
+	let mut moves = Vec::new();
+	
+	{
+		let row_offset = TileOffset::Negative(1);
+		let col_offset = TileOffset::Negative(1);
+		push_simple_move_if_valid(
+			board, row, col, &row_offset, &col_offset, &mut moves);
+	}
+	
+	{
+		let row_offset = TileOffset::Negative(1);
+		let col_offset = TileOffset::Positive(1);
+		push_simple_move_if_valid(
+			board, row, col, &row_offset, &col_offset, &mut moves);
+	}
+	
+	{
+		let row_offset = TileOffset::Positive(1);
+		let col_offset = TileOffset::Negative(1);
+		push_simple_move_if_valid(
+			board, row, col, &row_offset, &col_offset, &mut moves);
+	}
+	
+	{
+		let row_offset = TileOffset::Positive(1);
+		let col_offset = TileOffset::Positive(1);
+		push_simple_move_if_valid(
+			board, row, col, &row_offset, &col_offset, &mut moves);
+	}
+
+	moves
+}
+
+// checks if it is possible to make a simple move with the given row
+// and tile offset, and if so, adds the move to the vector
+fn push_simple_move_if_valid
+(board : &super::Board,
+		start_row : usize,
+		start_col : usize,
+		row_offset : &TileOffset,
+		col_offset : &TileOffset,
+		moves : &mut Vec<SimpleMove>) {
+	let tile_on_board = is_tile_offset_in_bounds(
+		board, start_row, start_col, &row_offset, &col_offset);
+	if tile_on_board {
+		let (offset_row, offset_col)
+			= offset_tile(start_row, start_col, &row_offset, &col_offset);
+		let tile = board.get_tile(offset_row, offset_col);
+		if tile.get_piece().is_none() {
+			let the_move = SimpleMove{
+				to_row : offset_row, to_col : offset_col };
+			moves.push(the_move);
+		}
+	}
+}
+
 // This enum describes an offset direction and magnitude.
 enum TileOffset {
 	Positive(usize),
@@ -23,8 +135,8 @@ fn offset_value
 fn offset_tile
 (start_row : usize,
 		start_col : usize,
-		col_offset : &TileOffset,
-		row_offset : &TileOffset)
+		row_offset : &TileOffset,
+		col_offset : &TileOffset)
 -> (usize, usize) {
 	(offset_value(start_row, row_offset),
 			offset_value(start_col, col_offset))
@@ -46,7 +158,7 @@ fn is_offset_value_in_range
 
 // checks if a tile on the board can be reached when
 // moving from one position on the board to another
-fn is_tile_offset_on_bounds
+fn is_tile_offset_in_bounds
 (board : &super::Board,
 		start_row : usize,
 		start_col : usize,
