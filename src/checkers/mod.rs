bae06ec05520a467116f4b2bf9c153db79340e64
Derek Boone <boonesoftware@gmail.com>
2015-08-22 17:41:45 -0400

    Removed `*` imports from `checkers` module
    These are bad style.

--- src/checkers/mod.rs
+++ src/checkers/mod.rs
@@ -1,24 +1,30 @@
 mod ai;
-pub use checkers::ai::*;
+pub use checkers::ai::{
+	Direction,
+	find_simple_moves_for_king,
+	find_jump_moves_for_king,
+	find_simple_moves_for_man,
+	find_jump_moves_for_man,
+	JumpMove,
+	SimpleMove};
 
 mod board;
-pub use checkers::board::*;
+pub use checkers::board::{Board, BoardPosition};
 
-pub mod display;
+mod display;
+pub use checkers::display::print_board;
 
 mod game;
-pub use checkers::game::Game;
-pub use checkers::game::GameState;
-pub use checkers::game::MoveError;
+pub use checkers::game::{Game, GameState, MoveError};
 
 mod input;
-pub use checkers::input::*;
+pub use checkers::input::parse_move;
 
 mod piece;
-pub use checkers::piece::*;
+pub use checkers::piece::{KingPiece, ManPiece, Piece, PieceType};
 
 mod player;
 pub use checkers::player::Player;
 
 mod tile;
-pub use checkers::tile::*;
+pub use checkers::tile::{EmptyTile, OccupiedTile, Tile};
