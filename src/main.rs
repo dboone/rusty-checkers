687af6dc4a3e08dcdf0443d1a2f480ec8922de85
Derek Boone <boonesoftware@gmail.com>
2015-08-22 22:53:50 -0400

    Created game loop
    * Assembled game logic, user input, and board output
    * Added error handling (errors are printed to `stdout` with nice
    messages)
    * Added support for quitting the application before an end game
    condition is reached

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 #[macro_use]
 extern crate corrosion;
-use std::io::stdout;
+
+use std::io::{stdin, stdout, Write};
 
 mod checkers;
 use checkers::{
 	BoardPosition,
 	Game,
 	GameState,
+	InputError,
 	MoveError,
-	SimpleMove};
+	SimpleMove,
+	TokenError};
 
 mod util;
 
@@ -16,7 +19,6 @@ fn apply_positions_as_move
 (game : &mut Game, positions : Vec<BoardPosition>)
 -> Result<GameState, MoveError> {
 	if positions.len() == 2 {
-		// simple move or jump move?
 		let start = positions[0];
 		let end = positions[1];
 		
@@ -34,16 +36,107 @@ fn apply_positions_as_move
 	}
 }
 
+enum PlayerColor {
+	Red,
+	Black
+}
+
+fn player_id_to_color(player_id : u32) -> PlayerColor {
+	match player_id {
+		1 => PlayerColor::Red,
+		2 => PlayerColor::Black,
+		_ => unreachable!()
+	}
+}
+
+fn player_color_to_name(color : PlayerColor) -> &'static str {
+	match color {
+		PlayerColor::Red => "Red",
+		PlayerColor::Black => "Black"
+	}
+}
+
 fn main() {
     println!("Welcome to Draughts!");
 
 	let mut writer = stdout();
 	let mut game = Game::new();
-
-	checkers::display::print_board(&mut writer, game.board()).unwrap();
-
-	let positions = vec![BoardPosition::new(2, 0), BoardPosition::new(3, 1)];
-	let move_result = apply_positions_as_move(&mut game, positions);
-
-	checkers::display::print_board(&mut writer, game.board()).unwrap();
+	let mut line = String::new();
+	
+	checkers::print_board(&mut writer, game.board()).unwrap();
+	
+	'game_loop: loop {
+		line.clear();
+		
+		let player_name = player_color_to_name(
+			player_id_to_color(
+				game.current_player().id));
+		
+		print!("\n{}'s move: ", player_name);
+		
+		for _ in stdout().flush().err() {
+			panic!("Something really bad happened!");
+		}
+		
+		for _ in stdin().read_line(&mut line).err() {
+			panic!("Something really bad happened!");
+		}
+		
+		let line = line.trim();
+		
+		match line {
+			"Q" | "q" => {
+				println!("\nGiving up so soon?");
+				break 'game_loop;
+			}
+			_ => { }
+		}
+		
+		let parse_result = checkers::parse_move(&line);
+		
+		match parse_result {
+			Ok(positions) => {
+				let move_result = apply_positions_as_move(&mut game, positions);
+				match move_result {
+					Ok(game_state) => match game_state {
+						GameState::InProgress => { },
+						GameState::GameOver{winner_id} => {
+							let player_name = player_color_to_name(
+								player_id_to_color(winner_id));
+							
+							println!("\nGame over! {} won!", player_name);
+							break 'game_loop;
+						}
+					},
+					Err(e) => match e {
+						MoveError::InvalidMove => println!("\n *** Illegal move"),
+						MoveError::ShouldHaveJumped => println!("\n *** Must take jump")
+					}
+				}
+			},
+			Err(e) => match e {
+				InputError::TooFewTokens =>
+					println!("\n *** You must specify at least two board positions"),
+				InputError::InvalidTokens{tokens : errors} => {
+					for error in errors {
+						match error {
+							TokenError::MissingFile{token} =>
+								println!("\n *** Board position '{}' must specify file", token),
+							TokenError::MissingRank{token} =>
+								println!("\n *** Board position '{}' must specify rank", token),
+							TokenError::ZeroRank{token} =>
+								println!("\n *** Rank cannot be zero: {}", token),
+							TokenError::InvalidCharacter{token, char_index} => {
+								let ch = token.chars().nth(char_index).unwrap();
+								println!("\n *** Board position '{}' contains invalid character '{}'", token, ch);
+							}
+						}
+					}
+				}
+			}
+		}
+		
+		println!("");
+		checkers::print_board(&mut writer, game.board()).unwrap();
+	}
 }
